d545b4387ea415098d77b333c1741f5c0887ffb0
Pauline Borges <pauline.borges18@gmail.com>
2024-01-03 16:16:55 +0100

    progess on the connection system

---
+++ FrontEnd/login.js
@@ -0,0 +1,41 @@
+export function ajoutListenerLogin() {
+    const formLogin = document.querySelector(".form-login");
+    formLogin.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const login = {
+        email: event.target.querySelector("[name=email]").value,
+        password: event.target.querySelector("[name=password]").value,
+    };
+    // Création de la charge utile au format JSON
+    const chargeUtile = JSON.stringify(login);
+
+    //Appel de la fonction fetch avec les informations nécessaires
+    fetch("http://localhost:5678/api/users/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: chargeUtile
+    });
+    then(response => {
+        // Vérifier si la requête a fonctionné (statut 200)
+        if (!response.ok) {
+            throw new Error("Erreur HTTP! Statut: ${response.status}`);")
+        }
+
+        // Si la requête est réussie, analyse de la réponse JSON
+        return response.json();
+    })
+    .then (data => {
+        if (data.success) {
+            window.location.href = "./index.html";
+        } else {
+            alert("Erreur dans l\'identifiant ou le mot de passe");
+        }
+    })
+    .catch(error => {
+        console.error("Erreur lors de l\'authentification", error);
+    })
+
+});
+
+}
