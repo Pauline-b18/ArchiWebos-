7eef6aab79fbab3e543714c18ac29e2f19090d21
Pauline Borges <pauline.borges18@gmail.com>
2024-01-23 18:34:22 +0100

    gallery factorization + dynamic modal form

--- FrontEnd/gallery.js
+++ FrontEnd/gallery.js
@@ -1,167 +1,126 @@
 // Fonction pour récupérer les données de l'API
-function fetchData(apiEndpoint) {
-    return fetch(apiEndpoint)
-        .then(response => response.json())
-        .catch(error => {
-            console.error('Erreur lors de la récupération des données:', error);
-        });
+async function fetchData(apiEndpoint) {
+    try {
+        const response = await fetch(apiEndpoint);
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Erreur lors de la récupération des données:', error);
+    }
 }
 
-
-// Appel de la fonction fetchData avec l'URL de l'API works
+// Récupération des 2 API (works/catégories)
 const worksPromise = fetchData("http://localhost:5678/api/works");
-
-// Appel de la fonction fetchData avec l'URL de l'API des catégories
 const categoriesPromise = fetchData("http://localhost:5678/api/categories");
 
-// Utiliser Promise.all pour attendre que les 2 appels soient terminés
+// Promise.all permet d'attendre que les 2 appels soient terminés
 Promise.all([worksPromise, categoriesPromise])
     .then(([worksData, categoriesData]) => {
-        // Organisation des données des catégories dans un objet
-        const categoriesById = {};
-        categoriesData.forEach(category => {
-            categoriesById[category.id] = category;
-        });
-        displayWorksWithCategories(worksData, categoriesById);  // Afficher les travaux avec les catégories
+        const categoriesById = categoriesData.reduce((acc, category) => {
+            acc[category.id] = category;
+            return acc;
+        }, {});
+        displayWorksWithCategories(worksData, categoriesById);
     })
     .catch(error => {
         console.error('Une erreur s\'est produite:', error);
     });
 
+// Fonction pour afficher les travaux filtrés
+const displayFilteredWorks = (filteredWorks, categoriesById) => {
+    const gallery = document.querySelector('.gallery');
+    gallery.innerHTML = '';
+
+    filteredWorks.forEach(work => {
+        gallery.appendChild(createWorkElement(work, categoriesById));
+    });
+};
+
+// Fonction pour créer un élément de travail
+const createWorkElement = (work, categoriesById) => {
+    const workContainer = document.createElement('div');
+    workContainer.classList.add('work');
+
+    const titleElement = document.createElement('h2');
+    titleElement.textContent = work.title;
+
+    const imageElement = document.createElement('img');
+    imageElement.src = work.imageUrl;
+    imageElement.alt = work.title;
+
+    const categoryElement = document.createElement('p');
+    const category = categoriesById[work.categoryId];
+    categoryElement.textContent = `Category: ${category ? category.name : 'Unknown'}`;
+
+    workContainer.appendChild(titleElement);
+    workContainer.appendChild(imageElement);
+    workContainer.appendChild(categoryElement);
+
+    return workContainer;
+};
+
 // Fonction pour afficher les travaux avec les catégories sur la page
-function displayWorksWithCategories(works, categoriesById) {
+const displayWorksWithCategories = (works, categoriesById) => {
     const gallery = document.querySelector('.gallery');
     const categoriesContainer = document.querySelector('.categories');
+    const categoriesList = document.createElement('ul'); 
 
-    // Parcourir chaque élément du tableau works
-    works.forEach(work => {
-        // Création d'éléments HTML
-        const workContainer = document.createElement('div');
-        workContainer.classList.add('work');
-
-        const titleElement = document.createElement('h2');
-        titleElement.textContent = work.title;
-
-        const imageElement = document.createElement('img');
-        imageElement.src = work.imageUrl;
-        imageElement.alt = work.title;
-
-        const categoryElement = document.createElement('p');
-        // Utilise l'ID de catégorie pour accéder à la catégorie correspondante
-        const category = categoriesById[work.categoryId];
-        categoryElement.textContent = `Category: ${category ? category.name : 'Unknown'}`;
-
-        // Ajout des éléments à workContainer
-        workContainer.appendChild(titleElement);
-        workContainer.appendChild(imageElement);
-        workContainer.appendChild(categoryElement);
-
-        // Ajout de workContainer à gallery
-        gallery.appendChild(workContainer);
-    });
-
-    // Afficher les catégories dans le conteneur des catégories
-    categoriesContainer.innerHTML = ''; // Effacer le contenu existant ??
-    const categoriesList = document.createElement('ul');
+    gallery.innerHTML = '';
+    categoriesContainer.innerHTML = '';
     categoriesContainer.appendChild(categoriesList);
 
-    // Création d'un élément li pour "Tous"
-    const allCategoryElement = document.createElement('li');
-    allCategoryElement.textContent = 'Tous';
-
-    // Ajout de l'événement de clic pour afficher tous les travaux
-    allCategoryElement.addEventListener('click', () => {
-        gallery.innerHTML = ''; // Effacer la galerie actuelle
-        displayFilteredWorks(works, categoriesById); // Afficher tous les travaux
-    });
-
-    // Ajout de allCategoryElement à categoriesList
-    categoriesList.appendChild(allCategoryElement);
-
-    // Parcourir chaque catégorie
-    Object.values(categoriesById).forEach(category => {
-        // Création d'éléments HTML pour les catégories
+    const createCategoryElement = (category) => {
         const categoryElement = document.createElement('li');
         categoryElement.textContent = category.name;
 
-        // Ajout de categoryElement à categoriesList
-        categoriesList.appendChild(categoryElement);
-
-        // Gestionnaire d'événements pour filtrer les travaux au clic de la catégorie
         categoryElement.addEventListener('click', () => {
-            const filteredWorks = works.filter(work => work.categoryId === category.id); // Filtrer les travaux en fonction de leur ID
-
-            gallery.innerHTML = ''; // Effacer la galerie actuelle
-
-            displayFilteredWorks(filteredWorks, categoriesById); // Afficher les travaux filtrés
+            const filteredWorks = works.filter(work => work.categoryId === category.id);
+            displayFilteredWorks(filteredWorks, categoriesById);
         });
+
+        categoriesList.appendChild(categoryElement);
+    };
+
+    const allCategoryElement = document.createElement('li');
+    allCategoryElement.textContent = 'Tous';
+
+    allCategoryElement.addEventListener('click', () => {
+        displayFilteredWorks(works, categoriesById);
     });
-}
 
-// Fonction pour afficher les travaux filtrés
- function displayFilteredWorks(filteredWorks, categoriesById) {
-    const gallery = document.querySelector('.gallery');
+    categoriesList.appendChild(allCategoryElement);
 
-    // Parcourir chaque travail filtré
-    filteredWorks.forEach(work => {
-        // Création d'éléments HTML pour les travaux
-        const workContainer = document.createElement('div');
-        workContainer.classList.add('work');
+    Object.values(categoriesById).forEach(createCategoryElement);
 
-        const titleElement = document.createElement('h2');
-        titleElement.textContent = work.title;
-
-        const imageElement = document.createElement('img');
-        imageElement.src = work.imageUrl;
-        imageElement.alt = work.title;
-
-        const categoryElement = document.createElement('p');
-        // Utiliser l'ID de catégorie pour accéder à la catégorie correspondante
-        const category = categoriesById[work.categoryId];
-        categoryElement.textContent = `Category: ${category ? category.name : 'Unknown'}`;
-
-        // Ajout des éléments à workContainer
-        workContainer.appendChild(titleElement);
-        workContainer.appendChild(imageElement);
-        workContainer.appendChild(categoryElement);
-
-        // Ajout de workContainer à gallery
-        gallery.appendChild(workContainer);
+    works.forEach(work => {
+        gallery.appendChild(createWorkElement(work, categoriesById));
     });
-}
+};
 
-
-//Changer le contenu de la page si l'utilisateur est connecté
-function adminLog() {
+// Fonction pour changer le contenu de la page si l'utilisateur est connecté
+const adminLog = () => {
     const logout = document.querySelector(".logout-button");
     const login = document.querySelector(".login-button");
     const barAdmin = document.querySelector(".black-bar");
-
     const btnModifier = document.querySelector(".modal-button");
     const editButton = document.querySelector(".modal");
     const categories = document.querySelector("#categories");
-  
-  
+
     if (localStorage.getItem('token')) {
-      logout.style.display = "block";
-      login.style.display = "none";
-      
-      barAdmin.style.display = "block";
-      btnModifier.style.display = "block";
-      editButton.style.display = "block";
-      categories.style.display = "none";
-      
-      
-  
+        logout.style.display = "block";
+        login.style.display = "none";
+        barAdmin.style.display = "block";
+        btnModifier.style.display = "block";
+        editButton.style.display = "block";
+        categories.style.display = "none";
     } else {
-      logout.style.display = "none";
-      login.style.display = "block";
-  
-      barAdmin.style.display = "none";
-      btnModifier.style.display = "none";
-      editButton.style.display = "none";
-      categories.style.display = "block";
-  
-    }}
-  
+        logout.style.display = "none";
+        login.style.display = "block";
+        barAdmin.style.display = "none";
+        btnModifier.style.display = "none";
+        editButton.style.display = "none";
+        categories.style.display = "block";
+    }
+};
+
 adminLog();
--- FrontEnd/modal.js
+++ FrontEnd/modal.js
@@ -72,13 +72,116 @@ document.addEventListener("DOMContentLoaded", function() { // évite l'ouverture
   closeModal();
 });
 
-// Fonction pour ouvrir la modal
+// Fonction pour créer dynamiquement la section "Ajout photo" avec son formulaire
+function createDynamicPhotoSection() {
+  const modalContent = document.querySelector('.modal-content');
+
+  // Création de la section "Ajout photo"
+  const ajoutPhotoHeading = document.createElement('h3');
+  ajoutPhotoHeading.textContent = 'Ajout photo';
+
+  const customFileUploadDiv = document.createElement('div');
+  customFileUploadDiv.classList.add('custom-file-upload');
+
+  const imgFormDiv = document.createElement('div');
+  imgFormDiv.classList.add('img-form');
+
+  const iconImgDiv = document.createElement('div');
+  iconImgDiv.classList.add('icon-img');
+
+  const imageIcon = document.createElement('i');
+  imageIcon.classList.add('fa-regular', 'fa-image');
+
+  const photoLabel = document.createElement('label');
+  photoLabel.htmlFor = 'photo';
+
+  const photoInput = document.createElement('input');
+  photoInput.type = 'file';
+  photoInput.id = 'photo';
+  photoInput.name = 'photo';
+  photoInput.accept = 'image/*';
+  photoInput.required = true;
+
+  const formatDiv = document.createElement('div');
+  formatDiv.classList.add('format');
+  formatDiv.textContent = 'jpg, png: 4mo max';
+
+  iconImgDiv.appendChild(imageIcon);
+  photoLabel.appendChild(photoInput);
+
+  imgFormDiv.appendChild(iconImgDiv);
+  imgFormDiv.appendChild(photoLabel);
+  imgFormDiv.appendChild(formatDiv);
+
+  customFileUploadDiv.appendChild(imgFormDiv);
+
+  // Création du formulaire
+  const workForm = document.createElement('form');
+  workForm.id = 'work-form';
+
+  const titreTravauxDiv = document.createElement('div');
+  titreTravauxDiv.classList.add('titre-travaux');
+
+  const titreLabel = document.createElement('label');
+  titreLabel.htmlFor = 'titre-work';
+  titreLabel.textContent = 'Titre';
+
+  const titreInput = document.createElement('input');
+  titreInput.type = 'text';
+  titreInput.id = 'titre-work';
+  titreInput.name = 'titre';
+  titreInput.required = true;
+
+  titreTravauxDiv.appendChild(titreLabel);
+  titreTravauxDiv.appendChild(titreInput);
+
+  const categorieTravauxDiv = document.createElement('div');
+  categorieTravauxDiv.classList.add('categorie-travaux');
+
+  const categorieLabel = document.createElement('label');
+  categorieLabel.htmlFor = 'categorie-work';
+  categorieLabel.textContent = 'Catégorie';
+
+  const categorieSelect = document.createElement('select');
+  categorieSelect.id = 'categorie-work';
+  categorieSelect.name = 'categorie-work';
+  categorieSelect.required = true;
+
+  categorieTravauxDiv.appendChild(categorieLabel);
+  categorieTravauxDiv.appendChild(categorieSelect);
+
+  const errorPostParagraph = document.createElement('p');
+  errorPostParagraph.classList.add('error-post');
+
+  const lineDiv = document.createElement('div');
+  lineDiv.classList.add('line');
+
+  const submitButton = document.createElement('button');
+  submitButton.id = 'submit';
+  submitButton.type = 'submit';
+  submitButton.classList.add('validation');
+  submitButton.textContent = 'Valider';
+
+  workForm.appendChild(titreTravauxDiv);
+  workForm.appendChild(categorieTravauxDiv);
+  workForm.appendChild(errorPostParagraph);
+  workForm.appendChild(lineDiv);
+  workForm.appendChild(submitButton);
+
+  // Ajouter les éléments à la modal
+  modalContent.appendChild(ajoutPhotoHeading);
+  modalContent.appendChild(customFileUploadDiv);
+  modalContent.appendChild(workForm);
+}
+
+
+// Modifier la fonction openModal pour organiser les éléments dans la modale
 function openModal() {
-  modal.style.display = "block";
+  modal.style.display = 'block';
 
-
-  // Appel de la fonction fetchWorksForModal
+  // Appel des 2 fonctions
   getWorksDataForModal();
+
 }
 
 // Fonction pour fermer la modal
